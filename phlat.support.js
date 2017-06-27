3fa0f35a13942de81a90c2927c61c61ca49385ef
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-27 18:35:01 +0800

    Updated server/client test.

--- phlat.support.js
+++ phlat.support.js
@@ -35,6 +35,10 @@
               			"module": "phlat",
               			"author": "Richeve S. Bebedor",
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+              				"Vinse Vinalon <vinsevinalon@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/phlat.git",
               			"test": "phlat-test.js",
               			"global": true
